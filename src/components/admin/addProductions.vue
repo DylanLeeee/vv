d1f8ecdfffa38cd5470fc60502f47774f669b895
DylanLeeee <412191806@qq.com>
2021-06-26 22:22:36 +0800

    添加商品

--- src/components/admin/addProductions.vue
+++ src/components/admin/addProductions.vue
@@ -1,7 +1,7 @@
 <template >
   <el-container class="content">
-    <div>
-      <div style="height: 50px">
+    <el-container>
+      <el-header style="height: 50px">
         <div style="float: right; margin-right: 40%">
           <el-button type="primary" round @click="dialogFormVisible = true"
             >新增</el-button
@@ -12,8 +12,8 @@
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </div>
-      </div>
-      <div>
+      </el-header>
+      <el-main>
         <el-table
           :data="
             tableData.slice(
@@ -21,20 +21,25 @@
               currentPage * pageSize
             )
           "
-          style="width: 100%"
+          style="width: 100%; "
+
         >
-          <el-table-column prop="date" label="日期"> </el-table-column>
-          <el-table-column prop="name" label="商品名"> </el-table-column>
-          <el-table-column prop="address" label="京东地址"> </el-table-column>
+          <el-table-column prop="imgUrl" label="图片链接" width="100px"> 
+            <template slot-scope="scope">
+            <img :src="scope.row.imgUrl" style="height: 100px"/>
+            </template>
+          </el-table-column>
+          <el-table-column  label="商品名" min-width="150px"> 
+            <template slot-scope="scope">
+                <a :href="scope.row.materialUrl" target="_blank" >{{scope.row.name}}</a>
+            </template>
+          </el-table-column>
           <el-table-column prop="unitPrice" label="单价"> </el-table-column>
           <el-table-column prop="bestPrice" label="活动价"> </el-table-column>
-          <el-table-column prop="imgUrl" label="图片链接"> </el-table-column>
           <el-table-column prop="startDate" label="开始日期"> </el-table-column>
           <el-table-column prop="endDate" label="结束日期"> </el-table-column>
           <el-table-column prop="catagory" label="类别"> </el-table-column>
           <el-table-column prop="isJdSale" label="自营"> </el-table-column>
-          <el-table-column prop="materialUrl" label="商品链接">
-          </el-table-column>
           <el-table-column fixed="right" label="操作" width="100">
             <template slot-scope="scope">
               <el-button
@@ -60,16 +65,16 @@
           >
           </el-pagination>
         </div>
-      </div>
-    </div>
+      </el-main>
+    </el-container>
     <div>
       <el-dialog title="新增商品" :visible.sync="dialogFormVisible" style="width:1100px">
         <el-form :model="form" >
           <el-form-item label="商品ID" :label-width="formLabelWidth">
-            <el-input v-model="form.skuid" placeholder="请输入京东商品ID" style="width:300px"></el-input>
+            <el-input v-model="form.skuid" placeholder="请输入京东商品ID" style="width:300px" required></el-input>
           </el-form-item>
           <el-form-item label="价格" :label-width="formLabelWidth">
-            <el-input v-model="form.bestPrice" placeholder="卖价" style="width:300px" ></el-input>
+            <el-input v-model="form.bestPrice" placeholder="卖价" style="width:300px" required></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -89,23 +94,9 @@ export default {
   name: "AddProductions",
   data() {
     return {
-      pageSize: 10, //每页多少条
+      pageSize: 4, //每页多少条
       currentPage: 1, // 当前页
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小5虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          unitPrice: "￥200",
-          bestPrice: "￥150",
-          catagory: "沙发",
-          isJdSale: "是",
-          materialUrl: "www.taobao.com",
-          imgUrl: "www.baidu.com",
-          startDate: "2021-06-30",
-          endDate: "2021-07-30",
-        },
-      ],
+      tableData: [],
       form: {
         skuid:'',
         bestPrice:''
@@ -131,12 +122,23 @@ export default {
       this.dialogFormVisible=false;
       //此处通过skuid传参调取api，去抓取京东商品信息
       let product_info = axios.post('http://127.0.0.1:5000/get_product_info',{'skuid':this.form.skuid,'bestPrice':this.form.bestPrice}).then(res=>{
-          return res.data.data
+          return res.data
+      }).catch(error=>{
+            this.$message(error.data)
       });
-      product_info.then(res=>{
-            console.log(res[0].goodsName);  //获得state里的数据，把数据赋值给data里的变量
-        })
+    product_info.then(data=>{
+        this.$message(data.msg)
+    })
     }
+  },
+  mounted:function(){
+      let tableDataP= axios.post('http://127.0.0.1:5000/get_goods_list',{'goodsName':''}).then(res=>{
+          return res.data.data
+      })
+      tableDataP.then(res=>{
+          this.tableData=res
+      })
+
   }
 };
 </script>
